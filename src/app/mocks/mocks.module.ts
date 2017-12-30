2c934c2f70118d2a15d44f5115d27992add4ba04
Ariel <ariel.henryson@gmail.com>
2017-12-30 20:27:36 +0200

    test fix

---
+++ src/app/mocks/mocks.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+
+
+import { MockComponent } from './mock.component'
+
+
+@NgModule({
+  declarations: [
+    MockComponent
+  ],
+  exports:      [ ]
+})
+export class MockModule {}
